8d22b8e0f38b5c45254ce2022924e29e23756a15
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-12-11 16:02:58 +0200

    Revised

--- PhotoCollageMaker/PhotoCollageTests.cs
+++ PhotoCollageMaker/PhotoCollageTests.cs
@@ -1,6 +1,8 @@
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Windows;
+using System.Drawing;
 
 namespace PhotoCollageMaker
 {
@@ -24,20 +26,52 @@ namespace PhotoCollageMaker
 
         }
 
-        [TearDown]  
-
-        public void Teardown() 
-        
-        { 
-            
-            driver.Quit();
-        
-        }
+       
 
         [Test]
-        public void Test1()
+        public void Test_MakeCollage()
         {
-            Assert.Pass();
+            var noThanksButton = driver.FindElementByName("No Thanks !");
+            noThanksButton.Click();
+
+            Thread.Sleep(3000);
+
+            var collageButton = driver.FindElementByAccessibilityId("BtnCollage");
+            collageButton.Click();
+
+            Thread.Sleep(3000);
+
+            var allLocations = driver.FindElementByName("All locations");
+            allLocations.Click();
+
+            Thread.Sleep(1000);
+
+            var inputField = driver.FindElementByName("Address");
+            inputField.Clear();
+            Thread.Sleep(2000);
+            inputField.SendKeys("D:\\Documents\\Pictures\\Collage Editor");
+            inputField.SendKeys(Keys.Enter);
+
+            var itemsView = driver.FindElementByName("Items View");
+            itemsView.SendKeys(Keys.Control + "a");
+
+
+            var openButton = driver.FindElementByClassName("Button");
+            openButton.Click();
+
+            Thread.Sleep(6000);
+
+            var buttonSticker = driver.FindElementByAccessibilityId("BtnSticker");
+            buttonSticker.Click();
+
+            var stickerConfirm = driver.FindElementByAccessibilityId("BtnStickerOk");
+            stickerConfirm.Click();
+
+            var saveButton = driver.FindElementByName("Save");
+            saveButton.Click();
+
+            var yesButton = driver.FindElementByName("Yes");
+            yesButton.Click();   
         }
     }
 }
